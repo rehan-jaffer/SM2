63a4d27fb8f86b612c4269a5f6e22b7454798db6
Ray Jaffer <ray@thelondonvandal.com>
2020-01-10 10:44:35 +0000

    added basic tests for sm2 algorithm

--- spec/sm2_spec.rb
+++ spec/sm2_spec.rb
@@ -3,6 +3,40 @@ RSpec.describe Sm2 do
     expect(Sm2::VERSION).not_to be nil
   end
 
+  describe "intervals" do
+
+    it "always returns 1 (day) for the first interval" do
+
+      repetition = 1
+      interval = Sm2.next_interval(repetition)
+      expect(interval).to eq 1
+
+    end
+
+    it "always returns 6 (days) for the second interval" do
+
+      repetition = 2
+      interval = Sm2.next_interval(repetition)
+      expect(interval).to eq 6
+
+    end
+
+    it "returns an ever-increasing value of intervals" do
+
+      intervals = []
+      e_factor = 2.5
+
+      2.upto(10) do |repetition|
+        intervals.push Sm2.next_interval(repetition, intervals.last, e_factor)
+      end
+
+      expect(intervals
+        .each_cons(2)
+        .map { |x,y| y <=> x }).to all eq 1
+    end
+
+  end
+
   describe "e-factor" do
 
     it "calculates a new e-factor based on the old" do
@@ -21,3 +55,4 @@ RSpec.describe Sm2 do
   end
 
 end
+
