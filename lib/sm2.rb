ee17c6b2bc8bdfcc79e61e577f4cbebd2f4b2d61
Ray Jaffer <ray@thelondonvandal.com>
2020-01-10 10:20:34 +0000

    renamed new_ef function to be more readable, also split into two functions and added e_function formula for SM2

--- lib/sm2.rb
+++ lib/sm2.rb
@@ -3,8 +3,22 @@ require "sm2/version"
 module Sm2
   class Error < StandardError; end
 
-  def self.new_ef(ef: 2.5, q:)
-    1
+  MAX_QUALITY = 5
+
+  def self.new_e_factor(old_e_factor: 2.5, quality:)
+    raise ArgumentError if quality >= 5 || quality < 0 
+
+    if old_e_factor < 1.3
+      e_variable = 1.3
+    else
+      e_variable = old_e_factor
+    end
+
+    return e_variable + e_function(quality)
+  end
+
+  def self.e_function(quality)
+    (0.1 - (MAX_QUALITY - quality)) * (0.08 + (MAX_QUALITY - quality) * 0.02)
   end
 
 end
--- spec/sm2_spec.rb
+++ spec/sm2_spec.rb
@@ -6,10 +6,16 @@ RSpec.describe Sm2 do
   describe "e-factor" do
 
     it "calculates a new e-factor based on the old" do
+      e_factor = Sm2.new_e_factor(quality: 1)
+      expect(e_factor).not_to be nil
+    end
 
-      ef = Sm2.new_ef(ef: 1, q: 1)
-      expect(ef).not_to be nil
+    it "raises an exception if supplied quality of the response is greater than 5" do
+      expect { Sm2.new_e_factor(quality: 6) }.to raise_error ArgumentError
+    end
 
+    it "raises an exception if supplied quality of the response is less than 0" do
+      expect { Sm2.new_e_factor(quality: -1) }.to raise_error ArgumentError
     end
 
   end
