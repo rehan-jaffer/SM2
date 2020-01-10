380c30bf3cdb3fbc40c0aa14968ba309ea5906dc
Ray Jaffer <ray@thelondonvandal.com>
2020-01-10 10:44:02 +0000

    added next_interval method

--- lib/sm2.rb
+++ lib/sm2.rb
@@ -4,6 +4,18 @@ module Sm2
   class Error < StandardError; end
 
   MAX_QUALITY = 5
+  ONE_DAY = 1
+  SIX_DAYS = 6
+
+  def self.next_interval(repetitions, last_interval=0, e_factor=2.5)
+
+    raise ArgumentError if repetitions <= 0
+    return ONE_DAY if repetitions == 1
+    return SIX_DAYS if repetitions == 2
+   
+    last_interval * e_factor    
+
+  end
 
   def self.new_e_factor(old_e_factor: 2.5, quality:)
     raise ArgumentError if quality >= 5 || quality < 0 
